4f3894375949b1b1d8825f2a6acb06c83fa9f635
santiago <arboledamartinezs222@gmail.com>
2021-07-19 13:13:05 -0500

    organizacon de codigo

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const appServer = express();
 const usuarios = require('./routers/Usuarios');
 const productos = require('./routers/Productos');
 const pedidos = require('./routers/Pedidos');
+const appServer = express();
 appServer.use(bodyParser.urlencoded({ extended: false }));
 appServer.use(bodyParser.json());
 
